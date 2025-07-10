7769ab6941e84841c94bb5d8f7fb6dd914ce9acc
Rishi Raj Singh Shekhawat <rishishekhawat381@gmail.com>
2025-07-11 00:29:03 +0530

    Update BotChat.jsx

--- client/src/components/BotChat.jsx
+++ client/src/components/BotChat.jsx
@@ -5,7 +5,7 @@ const BotChat = () => {
   const directLine = useMemo(
     () =>
       createDirectLine({
-        secret: '7pA5v1bYncsOKLRnDaX4myOJklKEb2h7SPTzHSSWxVt6Zyfta8TkJQQJ99BGACi5YpzAArohAAABAZBS45O5.M5r27Pavha4XgKIfQf8CVWMLpHjqygVOsramAHeCtXLPXfNMm6LeJQQJ99BGACi5YpzAArohAAABAZBS3Pr7'  }),
+        secret: '7pA5v1bYncsOKLRnDaX4myOJklKEbta8TkJQQJ99BGACi5YpzAArohAAABAZBS45O5.M5r27Pavha4XgKIfQf8CVWMLpHjqygVOsramAHeCtXLPXfNMm6LeJQQJ99BGACi5YpzAArohAAABAZBS3Pr7'  }),
     []
   );
 
